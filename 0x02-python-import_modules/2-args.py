df53cd67763f139d0e64b03ecfabde81dc726a3d
justxd22 <xdjust18@gmail.com>
2023-11-03 03:18:04 +0200

    init #3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+def hand_args():
+    x = len(sys.argv) - 1
+
+    if x == 0:
+        print("0 arguments.")
+    else:
+        print("{} {}:".format(x, 'argument' if x == 1 else 'arguments'))
+        for i in range(1, x + 1):
+            print(f"{i}: {sys.argv[i]}")
+
+if __name__ == "__main__":
+    hand_args()
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+def add_args():
+    x = len(argv) - 1
+    sum = 0
+
+    if x == 0:
+        print(0)
+        exit()
+    else:
+        for i in range(1, x + 1):
+            sum += int(argv[i])
+    print(sum)
+
+if __name__ == "__main__":
+    add_args()
