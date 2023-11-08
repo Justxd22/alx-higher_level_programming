0866fb5daf679bfd36a3cd11efc6d5feaf567af5
justxd22 <xdjust18@gmail.com>
2023-11-09 01:47:25 +0200

    fixx

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,4 @@
 def search_replace(my_list, search, replace):
     if not my_list:
         return
-    mc = list(my_list)
-    for x in mc:
-        if x == search:
-            mc[mc.index(x)] = replace
-    return mc
+    return [x if not x == search else replace for x in my_list]
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if not my_list:
-        return
-    z = []
-    sum = 0
-    for x in my_list:
-        if x not in z:
-            sum += x
-            z.append(x)
-    return sum
+    return sum(set(my_list))
