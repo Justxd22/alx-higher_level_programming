16ef2dc6637acae4826cac098e47ec4907b9356b
Justxd22 <xdjust18@gmail.com>
2023-11-28 19:11:04 +0200

    fix

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """init."""
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
         Rectangle.number_of_instances += 1
 
     @property
