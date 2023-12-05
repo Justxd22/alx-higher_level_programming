905617923f2cecebef56fe7bb219b9b3e69ef960
Justxd22 <xdjust18@gmail.com>
2023-12-05 02:02:24 +0200

    dfff

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,6 +10,6 @@ def inherits_from(obj, a_class):
     - obj: object to lookup
     - a_class: class to compare
     """
-    if isinstance(type(obj), a_class):
+    if type(obj) is a_class:
         return False
     return issubclass(type(obj), a_class)
