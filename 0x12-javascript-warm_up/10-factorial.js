6baaf52bf4c6729071eb564a3c8551951030ffef
Justxd22 <xdjust18@gmail.com>
2024-03-13 23:44:35 +0200

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,6 +2,6 @@
 const n = isNaN(Number(process.argv[2])) ? 1 : Number(process.argv[2]);
 function factor (x) {
   if (x === 0) return 1;
-  return x * fact(x - 1);
+  return x * factor(x - 1);
 }
-console.log(fact(n));
+console.log(factor(n));
