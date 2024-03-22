998e03ecec14c764ce0a739dbe1224564011c840
Justxd22 <xdjust18@gmail.com>
2024-03-23 01:12:03 +0200

    fff

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,16 +4,16 @@
 
 def main(args):
     """Py sql."""
-    if len(args) < 5:
-        raise Exception("pass args please")
     db = MySQLdb.connect(host='localhost', user=args[1],
                          passwd=args[2], db=args[3])
     cur = db.cursor()
     cur.execute("SELECT c.id, c.name, s.name FROM cities c\
                 JOIN states s ON s.id=c.state_id where s.name=%s\
-                ORDER BY c.id", args[4])
+                ORDER BY c.id", (sys.argv[4],))
     states = cur.fetchall()
     print(", ".join(map(lambda x: "%s" % x, states)))
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
