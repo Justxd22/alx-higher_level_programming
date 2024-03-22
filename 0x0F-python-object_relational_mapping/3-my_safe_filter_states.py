304f58f7d48703dd53447d8f4db829797a2e29aa
Justxd22 <xdjust18@gmail.com>
2024-03-23 01:05:28 +0200

    ffff

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Py sql."""
+import MySQLdb
+import sys
 
 
 def main(args):
@@ -7,16 +9,16 @@ def main(args):
     if len(args) < 5:
         raise Exception("pass args please")
     db = MySQLdb.connect(host='localhost', user=args[1],
-                         passwd=args[2], db=args[3])
+                         passwd=args[2], db=args[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name like %s\
-                ORDER BY id ASC", args[4])
+    cur.execute("SELECT * FROM states WHERE name LIKE %s\
+                ORDER BY id ASC", (match, ))
     states = cur.fetchall()
     for state in states:
         print(state)
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
     main(sys.argv)
