bb241a600ffb60f4c494cb8de28eee3e312841bf
Justxd22 <xdjust18@gmail.com>
2024-03-23 00:03:34 +0200

    fixx

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Py sql."""
+import sys
+import MySQLdb
 
 
 def main(args):
@@ -7,15 +9,15 @@ def main(args):
     if len(args) < 4:
         raise Exception("pass args please")
     db = MySQLdb.connect(host='localhost', user=args[1],
-                         passwd=args[2], db=args[3])
+                         passwd=args[2], db=args[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
     for state in states:
         print(state)
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
     main(sys.argv)
