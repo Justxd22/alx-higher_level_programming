1a1e8fa20b810af8e9b431b4d8d0c464d1c559d9
Justxd22 <xdjust18@gmail.com>
2024-03-23 01:34:32 +0200

    fixx

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,5 +11,5 @@ class State(Base):
     """Inherted from Base."""
 
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
