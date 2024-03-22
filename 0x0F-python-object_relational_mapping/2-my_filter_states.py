08d5cf8c524b56a2a5ab923ab206b0beb667cbab
Justxd22 <xdjust18@gmail.com>
2024-03-23 01:01:11 +0200

    fff

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ def main(args):
     db = MySQLdb.connect(host='localhost', user=args[1],
                          passwd=args[2], db=args[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name like '{}'\
+    cur.execute("SELECT * FROM states WHERE name like BINARY '{}'\
                  ".format(args[4]))
     states = cur.fetchall()
     for state in states:
