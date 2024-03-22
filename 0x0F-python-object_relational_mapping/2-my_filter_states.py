71aa2db755aaf69ea55edef632e58a59b724caba
Justxd22 <xdjust18@gmail.com>
2024-03-23 00:53:55 +0200

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """Py sql."""
-
+import MySQLdb
+import sys
 
 def main(args):
     """Py sql."""
-    if len(args) < 5:
-        raise Exception("pass args please")
     db = MySQLdb.connect(host='localhost', user=args[1],
-                         passwd=args[2], db=args[3])
+                         passwd=args[2], db=args[3], port=3306)
     cur = db.cursor()
     cur.execute(f"SELECT * FROM states WHERE name like '{args[4]}'\
                 ORDER BY id ASC")
     states = cur.fetchall()
     for state in states:
         print(state)
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
     main(sys.argv)
