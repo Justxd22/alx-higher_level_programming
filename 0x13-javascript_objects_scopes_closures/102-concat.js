c4beac9861690d8c59be789e5188a7c1be05f58c
Justxd22 <xdjust18@gmail.com>
2024-03-23 02:47:25 +0200

    ffff

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function concatFiles (file1, file2, destination) {
-  fs.readFile(file1, 'utf8', (err, data1) => {
-    if (err) throw err;
-
-    fs.readFile(file2, 'utf8', (err, data2) => {
-      if (err) throw err;
-
-      const concatenatedContent = data1 + data2;
-
-      fs.writeFile(destination, concatenatedContent, (err) => {
-        if (err) throw err;
-        console.log(`Files ${file1} and ${file2} concatenated successfully to ${destination}`);
-      });
-    });
-  });
-}
-
-const [, , file1, file2, destination] = process.argv;
-concatFiles(file1, file2, destination);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,29 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof h === 'undefined' || typeof h === 'undefined') { return; }
-    this.width = w >= 0 ? w : NaN;
-    this.height = h >= 0 ? h : NaN;
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const x = this.width;
-    this.width = this.height;
-    this.height = x;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
+const Square = require("./4-rectangle");
 
 class square extends Rectangle {
   constructor (size) {
@@ -31,4 +8,4 @@ class square extends Rectangle {
   }
 }
 
-module.exports = Rectangle;
+module.exports = Square;
