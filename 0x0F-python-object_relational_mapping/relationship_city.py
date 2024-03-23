a4dfa743780c32cde0d01c059fa90a5bd2225ae4
Justxd22 <xdjust18@gmail.com>
2024-03-23 02:14:42 +0200

    fff

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Fetch all using orm in sqlalchemy."""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_state import Base, State
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """
+    Inherted from Base.
+
+    Using orm.
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State")
