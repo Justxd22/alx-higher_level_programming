ded0d37db1e3c6b31fccc95e9c3f7bcafb1c5a56
Justxd22 <xdjust18@gmail.com>
2024-03-29 04:36:09 +0200

    oii

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,9 +5,6 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as res:
-        print(res.headers.get('X-Request-Id'))
-
     data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as re:
