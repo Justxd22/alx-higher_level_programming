c6f3f767203bee3b18d8e1830e065bd2e8368936
justxd22 <xdjust18@gmail.com>
2024-04-24 03:47:15 +0200

    iiii

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(link, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body);
+    printCharacters(characters.characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
