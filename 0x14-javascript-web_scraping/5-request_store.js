b41c4e40245ec5751da15486b6e71acce584018b
Justxd22 <xdjust18@gmail.com>
2024-04-24 23:06:31 +0200

    dddd

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const link = process.argv[2];
+const file = process.argv[3];
+
+request(link, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  fs.writeFile(file, body, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
